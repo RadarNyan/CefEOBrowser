4fddd1fba75b05e6a7e59b8af6bde7fe94128769
RadarNyan <RadarNyan@moe9th.com>
2018-08-18 15:45:55 +0900

    Disable gpu for better performance
    * yeah... just let CPU do the work.

--- CefEOBrowser/FormBrowser.cs
+++ CefEOBrowser/FormBrowser.cs
@@ -37,6 +37,7 @@ namespace CefEOBrowser
             settings.BrowserSubprocessPath = Path.Combine(cef_path, @"bin\CefSharp.BrowserSubprocess.exe");
             settings.LogSeverity = LogSeverity.Disable;
             settings.CefCommandLineArgs.Add("proxy-server", proxy);
+            settings.DisableGpuAcceleration();
             Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
 
             Browser = new ChromiumWebBrowser(url)
